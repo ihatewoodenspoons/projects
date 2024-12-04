d07b87afd247943b46ada27539694a6b50d002ef
ihatewoodenspoons <jesusmorontaramos@gmail.com>
2024-12-04 01:00:50 -0500

    Removed a line.
    Removed "using namespace std;"

--- calculator/current & compiled/src/calculator.cpp
+++ calculator/current & compiled/src/calculator.cpp
@@ -1,105 +1,100 @@
-
 #include <iostream>
 #include <cmath>
 #include <chrono>
 #include <thread>
 #include <limits> // For numeric_limits
 
-using namespace std;
-
 int main() {
     double num1, num2, num3, result;
-    string operation, trigOperation, angleType;
+    std::string operation, trigOperation, angleType;
     char answer;
 
-    cout << "Basic Terminal Calculator v0.17, macOS build. ";
-    cout << "Cleared Display.";
+    std::cout << "Basic Terminal Calculator v0.1, macOS build. ";
+    std::cout << "Cleared Display.";
     system("clear");
 
     while (true) {
         // First Number
-        cout << "\nEnter first number:";
-        cin >> num1;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer
+        std::cout << "\nEnter first number:";
+        std::cin >> num1;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear buffer
 
         // Second Number
-        cout << "\nEnter second number:";
-        cin >> num2;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer
+        std::cout << "\nEnter second number:";
+        std::cin >> num2;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear buffer
 
         // Third Number
-        cout << "\nEnter third number used for square roots, trigonometric operations:";
-        cin >> num3;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer
+        std::cout << "\nEnter third number used for square roots, trigonometric operations:";
+        std::cin >> num3;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear buffer
 
         // Asking for operation
-        cout << "What operation? Should be +, -, *, /, ^, trig, or root (square root). Type 'help' for help.:";
-        getline(cin, operation); // Use getline for full line input
+        std::cout << "What operation? Should be +, -, *, /, ^, trig, or root (square root). Type 'help' for help.:";
+        std::getline(std::cin, operation); // Use getline for full line input
 
         if (operation == "help") {
-            cout << "\n- HELP GUIDE:";
-            cout << "\n- Enter numbers for operations like addition, subtraction, multiplication, or division.";
-            cout << "\n- Operations available: +, -, *, /, ^ (power), root (square root).";
-            cout << "\n- For square roots, enter the number for which you want the square root or trig. operations (third number).";
-            cout << "\n- For all operations, excluding square roots, the first and second numbers are used.";
-            cout << "\n- Enter 'help' at any time to show this guide.";
-            cout << "\n- Returning you to the beginning.";
+            std::cout << "\n- HELP GUIDE:";
+            std::cout << "\n- Enter numbers for operations like addition, subtraction, multiplication, or division.";
+            std::cout << "\n- Operations available: +, -, *, /, ^ (power), root (square root).";
+            std::cout << "\n- For square roots, enter the number for which you want the square root or trig. operations (third number).";
+            std::cout << "\n- For all operations, excluding square roots, the first and second numbers are used.";
+            std::cout << "\n- Enter 'help' at any time to show this guide.";
+            std::cout << "\n- Returning you to the beginning.";
             continue;
         }
-
-       
 
         // Additional operations
         if (operation == "root") {
             if (num3 < 0) {
-                cout << "\nComplex numbers are not supported yet. Returning to beginning.";
+                std::cout << "\nComplex numbers are not supported yet. Returning to beginning.";
                 continue;
             }
-            result = sqrt(num3);
+            result = std::sqrt(num3);
         } else if (operation == "trig"){
-            cout << "Use degrees or radians? (deg or rad):";
-    cin >> angleType;
-    cin.ignore();
-    cout << angleType;
+            std::cout << "Use degrees or radians? (deg or rad):";
+            std::cin >> angleType;
+            std::cin.ignore();
+            std::cout << angleType;
 
-    cout << "Enter a trigonometric function (sine, cosine, tangent). (sin, cos, tan)";
-    cin >> trigOperation;
-    cin.ignore();
-    cout << trigOperation;
+            std::cout << "Enter a trigonometric function (sine, cosine, tangent). (sin, cos, tan)";
+            std::cin >> trigOperation;
+            std::cin.ignore();
+            std::cout << trigOperation;
 
-     if (trigOperation == "sin") {
-        if (angleType == "deg") {
-            result = sin(num3 * M_PI / 180 );
-        } else if (angleType == "rad") {
-            result = sin(num3); // Uses radians by default.
-        } else {
-            cout << "Invalid input for angle type or input error. Returning to beginning.";
-            continue;
-        }
-    } else if (trigOperation == "cos") {
-        if (angleType == "deg") {
-            result = cos(num3 * M_PI / 180);
-        } else if (angleType == "rad") {
-            result = cos(num3); // Uses radians by default.
-        } else {
-            cout << "Invalid input for angle type. Returning to beginning.";
-            continue;
-        }
-    } else if (trigOperation == "tan") {
-        if (angleType == "deg") {
-            result = tan(num3 * M_PI / 180);
-        } else if (angleType == "rad") {
-            result = tan(num3);
-        } else {
-            cout << "Invalid input for angle type or input error. Returning to beginning.";
-            continue;
+            if (trigOperation == "sin") {
+                if (angleType == "deg") {
+                    result = std::sin(num3 * M_PI / 180 );
+                } else if (angleType == "rad") {
+                    result = std::sin(num3); // Uses radians by default.
+                } else {
+                    std::cout << "Invalid input for angle type or input error. Returning to beginning.";
+                    continue;
+                }
+            } else if (trigOperation == "cos") {
+                if (angleType == "deg") {
+                    result = std::cos(num3 * M_PI / 180);
+                } else if (angleType == "rad") {
+                    result = std::cos(num3); // Uses radians by default.
+                } else {
+                    std::cout << "Invalid input for angle type. Returning to beginning.";
+                    continue;
+                }
+            } else if (trigOperation == "tan") {
+                if (angleType == "deg") {
+                    result = std::tan(num3 * M_PI / 180);
+                } else if (angleType == "rad") {
+                    result = std::tan(num3);
+                } else {
+                    std::cout << "Invalid input for angle type or input error. Returning to beginning.";
+                    continue;
+                }
+            } else {
+                std::cout << "Invalid trigonometric function. Returning to beginning.";
+                continue; // Error handling
             }
-    } else {
-        cout << "Invalid trigonometric function. Returning to beginning.";
-        continue; // Error handling
-    }
         } else if (operation == "^") {
-            result = pow(num1, num2);
+            result = std::pow(num1, num2);
         } else if (operation == "+") {
             result = num1 + num2;
         } else if (operation == "-") {
@@ -108,32 +103,32 @@ int main() {
             result = num1 * num2;
         } else if (operation == "/") {
             if (num2 == 0) {
-                cout << "\nDividing by zero is not allowed. Returning to beginning.";
+                std::cout << "\nDividing by zero is not allowed. Returning to beginning.";
                 continue;
             }
             result = num1 / num2;
         } else {
-            cout << "\nInvalid operation. Returning to beginning.";
+            std::cout << "\nInvalid operation. Returning to beginning.";
             continue;
         }
 
         // Display result
         system("clear");
-        cout << "\nFirst number was: " << num1;
-        cout << "\nSecond number was: " << num2;
-        cout << "\nThird number was: " << num3;
-        cout << "\nResult is: " << result;
+        std::cout << "\nFirst number was: " << num1;
+        std::cout << "\nSecond number was: " << num2;
+        std::cout << "\nThird number was: " << num3;
+        std::cout << "\nResult is: " << result;
 
         // Continue or exit
-        cout << "\nDo you want to perform another calculation? (y/n):";
-        cin >> answer;
+        std::cout << "\nDo you want to perform another calculation? (y/n):";
+        std::cin >> answer;
         if (answer != 'y' && answer != 'Y') break;
 
         system("clear");
     }
 
-    cout << "System returned error code 0.";
-    cout << "\n";
+    std::cout << "System returned error code 0.";
+    std::cout << "\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     return 0;
 }
